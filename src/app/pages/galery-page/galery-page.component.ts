460a5689b9cdf48d0e391dbdff3be0d799c963cf
Maxim Efremov <maxefremov04092004@gmail.com>
2025-03-27 17:29:37 +0300

    add slider

--- src/app/pages/galery-page/galery-page.component.ts
+++ src/app/pages/galery-page/galery-page.component.ts
@@ -7,4 +7,25 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrl: './galery-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class GaleryPageComponent {}
+export class GaleryPageComponent {
+  public imgs = [
+    'assets/img1.png',
+    'assets/img2.png',
+    'assets/img3.png',
+    'assets/img4.png',
+    'assets/img5.png',
+    'assets/img6.png',
+    'assets/img7.png',
+  ];
+
+  public currentIndex = 0;
+
+  public nextSlide() {
+    this.currentIndex = (this.currentIndex + 1) % this.imgs.length;
+  }
+
+  public prevSlide() {
+    this.currentIndex =
+      (this.currentIndex - 1 + this.imgs.length) % this.imgs.length;
+  }
+}
