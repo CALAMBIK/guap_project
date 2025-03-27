7fdba66c17f0da3b79fd2789e5cc80b14abb1cb2
Maxim Efremov <maxefremov04092004@gmail.com>
2025-03-27 16:28:28 +0300

    add-proplems-page

--- src/app/pages/implementation-problems-page/implementation-problems-page.component.ts
+++ src/app/pages/implementation-problems-page/implementation-problems-page.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ProblemsService } from '../../services/problems.service';
+import { TechFeature } from '../../models/tech-feature';
+import { NgFor } from '@angular/common';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-implementation-problems-page',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './implementation-problems-page.component.html',
-  styleUrl: './implementation-problems-page.component.scss'
+  styleUrl: './implementation-problems-page.component.scss',
 })
 export class ImplementationProblemsPageComponent {
+  private readonly problemsService = inject(ProblemsService);
+  public problems: TechFeature[] = this.problemsService.smartCityProblems();
 
+  ngOnInit() {
+    this.renderChart();
+  }
+
+  renderChart() {
+    new Chart('problemChart', {
+      type: 'bar',
+      data: {
+        labels: [
+          'Безопасность',
+          'Затраты',
+          'Инфраструктура',
+          'Конфиденциальность',
+          'Совместимость',
+        ],
+        datasets: [
+          {
+            label: 'Популярность проблем',
+            data: [40, 80, 60, 40, 50],
+            backgroundColor: ['blue', 'yellow', 'red', 'green', 'orange'],
+          },
+        ],
+      },
+    });
+  }
 }
